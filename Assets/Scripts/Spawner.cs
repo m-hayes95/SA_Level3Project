4840e8487b41894b5a8e46441b45dd4e81036f45
Mike Hayes <michaelhayes322@gmail.com>
2025-12-14 02:41:32 +0000

    Spawner only spawns enemies if player is within range

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,20 +6,48 @@ using Random = UnityEngine.Random;
 public class Spawner : MonoBehaviour
 {
     public GameObject objectToSpawn;
-    public float spawnTimer;
-    public float spawnRange;
+    [Range(0.0f, 10.0f)] public float spawnTimer;
+    [Range(1.0f,50.0f)] public float spawnRange;
+    [Tooltip("How far the player can be from the spawner to allow spawning"), Range(1.0f,500.0f)] 
+    public float allowSpawnPlayerDistance = 50.0f;
 
     private Vector3 randSpawnPoint;
     private Vector3 lastSpawnPoint;
     private Vector3 center;
     private bool isSpawning = false;
+    private Player player;
+    private float distanceToPlayer;
 
+    private void Start()
+    {
+        player = (Player)FindFirstObjectByType(typeof(Player));
+    }
     private void Update()
     {
-        if (isSpawning) return;
+        if (isSpawning || !CheckCanSpawn()) return;
         
         StartCoroutine(SpawnNextObject());
     }
+
+    private bool CheckCanSpawn()
+    {
+        bool canSpawn = true;
+        distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+        
+        if (distanceToPlayer > allowSpawnPlayerDistance)
+        {
+            canSpawn = false;
+            Debug.Log($"{name} : Player distance from spawner = {distanceToPlayer} , can spawn = {canSpawn}");
+        }
+        else
+        {
+            canSpawn = true;
+            Debug.Log($"{name} : Player distance from spawner = {distanceToPlayer} , can spawn = {canSpawn}");
+        }
+        
+        return canSpawn;
+    }
+    
 
     private Vector3 GetSpawnPosition()
     {
@@ -46,5 +74,6 @@ public class Spawner : MonoBehaviour
         
         Vector3 spawnArea = new Vector3(spawnRange * 2, 1, spawnRange * 2);
         Gizmos.DrawWireCube(transform.position, spawnArea);
+        Gizmos.DrawWireSphere(transform.position, allowSpawnPlayerDistance);
     }
 }
